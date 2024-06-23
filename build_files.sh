7da6de2300ca9abad4c8543b87e52044abe629dd
aieducation1 <aieducationcarrerrecommend@gmail.com>
2024-06-24 02:40:13 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 echo " BUILD START"
 # Install dependencies
-python3.11.7 -m pip install -r requirement.txt
+python -m pip install -r requirement.txt
 
 # Collect static files
-python3.11.7 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 # Migrate the database
-python3.11.7 manage.py migrate
+python manage.py migrate
 
 echo " BUILD END"
