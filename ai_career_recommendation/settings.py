de1cce1d7eec569eaf0e7c6e35b530289e2e54bd
aieducation1 <aieducationcarrerrecommend@gmail.com>
2024-06-24 04:44:10 +0500

    Update settings.py

--- ai_career_recommendation/settings.py
+++ ai_career_recommendation/settings.py
@@ -73,12 +73,6 @@ WSGI_APPLICATION = 'ai_career_recommendation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
