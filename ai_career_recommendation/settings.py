f24d8f9a13140b9a03fe94a7d4e4b262efc57993
aieducation1 <aieducationcarrerrecommend@gmail.com>
2024-06-24 02:39:37 +0500

    Update settings.py

--- ai_career_recommendation/settings.py
+++ ai_career_recommendation/settings.py
@@ -9,7 +9,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,7 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
